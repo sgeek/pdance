fb1558aab0d31690f227f41eafc5c2c9a62b2a36
Pat Heddles <pat@pheddles.com>
2020-02-24 13:29:39 +1100

    Add admin index page with simple list of links.

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<?php
+require "..\main.php";
+
+$title = "Admin";
+require "head.php";
+
+?>
+<ul style="max-width:5em; margin-left:auto; margin-right:auto;">
+	<li><a href="city/">City</a></li>
+	<li><a href="comp/">Comp</a></li>
+	<li><a href="dancer/">Dancer</a></li>
+	<li><a href="entry/">Entry</a></li>
+	<li><a href="event/">Event</a></li>
+	<li><a href="level/">Level</a></li>
+	<li><a href="performance_type/">pType</a></li>
+	<li><a href="round/">Round</a></li>
+	<li><a href="school/">School</a></li>
+	<li><a href="video/">Video</a></li>
+</ul>
